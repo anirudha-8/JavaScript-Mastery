964dd28b5eecc17e204ac1d71c06a50eb71003bf
Anirudha <beleanirudha8@gmail.com>
2024-07-13 22:54:13 +0530

    write solution to day-1 tasks

---
+++ day - 1/dayOneSolution.js
@@ -0,0 +1,52 @@
+// Activity One : Variable Declaration
+
+// --> task - 1 <--
+var num = 8;
+console.log(num);
+
+// --> task - 2 <--
+let name = "Anirudha";
+console.log(name);
+
+// Activity Two : Constant Declaration
+
+// --> task - 3 <--
+const isValid = true;
+console.log(isValid);
+
+// Activity Three : Data Types
+
+// --> task - 4 <--
+let numberVariable = 4;
+console.log(typeof numberVariable);
+
+let stringVariable = "rohit";
+console.log(typeof stringVariable);
+
+let booleanVariable = false;
+console.log(typeof booleanVariable);
+
+let objectVariable = {
+	key1: "value1",
+	typeof: "value2",
+};
+console.log(typeof objectVariable);
+
+let arrayVariable = [1, 2.3, "a", "b", true, false, "bele"];
+console.log(typeof arrayVariable);
+
+// Activity Four : Reassigning Variables
+
+// --> Task - 5 <--
+let val = "old";
+console.log(val);
+val = "new";
+console.log(val);
+
+// Activity Five : Understanding const
+
+// --> Task - 6 <--
+
+const username = "anya";
+username = "rohya";
+console.log(username);
